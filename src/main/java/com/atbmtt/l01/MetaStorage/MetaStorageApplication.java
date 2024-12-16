d825e5723e20e3b9a052b8c92f64d6fcca656793
Dao Huy Vu <huyvu160102@gmail.com>
2024-12-16 10:33:17 +0700

    Initial commit

---
+++ src/main/java/com/atbmtt/l01/MetaStorage/MetaStorageApplication.java
@@ -0,0 +1,13 @@
+package com.atbmtt.l01.MetaStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atbmtt/l01/MetaStorage/MetaStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atbmtt.l01.MetaStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
